e1d0bf0080c035ccf3ec017c769713b9dcb5965a
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2022-09-15 13:26:06 -0400

    Component-testbed initial version

---
+++ src/app/app.module.ts
@@ -0,0 +1,85 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutsComponent } from './layouts/layouts.component';
+import { PositionComponent } from './position/position.component';
+import { TabLayoutComponent } from './tab-layout/tab-layout.component';
+import { IgxTabsModule, IgxIconModule, IgxCardModule, IgxButtonModule, IgxRippleModule, IgxCalendarModule, IgxGridModule, IgxInputGroupModule, IgxAvatarModule, IgxExpansionPanelModule, IgxListModule, IgxCheckboxModule, IgxDividerModule, IgxActionStripModule, IgxDateTimeEditorModule, IgxDatePickerModule, IgxChipsModule, IgxSliderModule, IgxButtonGroupModule, IgxSwitchModule, IgxRadioModule, IgxSelectModule, IgxComboModule, IgxToggleModule, IgxDropDownModule, IgxDialogModule, IgxSnackbarModule, IgxBannerModule, IgxNavbarModule, IgxNavigationDrawerModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { ExpansionPanelsComponent } from './expansion-panels/expansion-panels.component';
+import { CardCalendarComponent } from './card-calendar/card-calendar.component';
+import { ListsDataComponent } from './lists-data/lists-data.component';
+import { DataGridLayoutsComponent } from './data-grid-layouts/data-grid-layouts.component';
+import { ChartsComponent } from './charts/charts.component';
+import { IgxCategoryChartModule, IgxPieChartModule } from 'igniteui-angular-charts';
+import { TypeScaleComponent } from './type-scale/type-scale.component';
+import { InputsComponent } from './inputs/inputs.component';
+import { PickersComboComponent } from './pickers-combo/pickers-combo.component';
+import { DialogsAndOverlaysComponent } from './dialogs-and-overlays/dialogs-and-overlays.component';
+import { ImageComponent } from './image/image.component';
+import { ChildViewComponent } from './child-view/child-view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutsComponent,
+    PositionComponent,
+    TabLayoutComponent,
+    ExpansionPanelsComponent,
+    CardCalendarComponent,
+    ListsDataComponent,
+    DataGridLayoutsComponent,
+    ChartsComponent,
+    TypeScaleComponent,
+    InputsComponent,
+    PickersComboComponent,
+    DialogsAndOverlaysComponent,
+    ImageComponent,
+    ChildViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    HammerModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxTabsModule,
+    IgxIconModule,
+    IgxCardModule,
+    IgxButtonModule,
+    IgxRippleModule,
+    IgxCalendarModule,
+    IgxGridModule,
+    IgxInputGroupModule,
+    FormsModule,
+    IgxAvatarModule,
+    IgxExpansionPanelModule,
+    IgxListModule,
+    IgxCheckboxModule,
+    IgxDividerModule,
+    IgxActionStripModule,
+    IgxCategoryChartModule,
+    IgxPieChartModule,
+    IgxDateTimeEditorModule,
+    IgxDatePickerModule,
+    IgxChipsModule,
+    IgxSliderModule,
+    IgxButtonGroupModule,
+    IgxSwitchModule,
+    IgxRadioModule,
+    IgxSelectModule,
+    IgxComboModule,
+    IgxToggleModule,
+    IgxDropDownModule,
+    IgxDialogModule,
+    IgxSnackbarModule,
+    IgxBannerModule,
+    IgxNavbarModule,
+    IgxNavigationDrawerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
