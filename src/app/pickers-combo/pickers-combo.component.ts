265d5ad979b132ad3fc106ec94e4e20129a6fdcd
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2022-11-07 13:28:58 -0500

    Component-test (app scenarios) updates

--- src/app/pickers-combo/pickers-combo.component.ts
+++ src/app/pickers-combo/pickers-combo.component.ts
@@ -8,7 +8,6 @@ import { NorthwindService } from '../services/northwind.service';
 })
 export class PickersComboComponent implements OnInit {
   public northwindEmployees: any = null;
-  public northwindCategories: any = null;
 
   constructor(
     private northwindService: NorthwindService,
@@ -17,6 +16,5 @@ export class PickersComboComponent implements OnInit {
   ngOnInit() {
     // depending on implementation, data subscriptions might need to be unsubbed later
     this.northwindService.getData('Employees').subscribe(data => this.northwindEmployees = data);
-    this.northwindService.getData('Categories').subscribe(data => this.northwindCategories = data);
   }
 }
